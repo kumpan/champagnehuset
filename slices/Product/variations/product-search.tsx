50076752d073aafd6b006df19eb6da7b6a511a30
Emilito <144819695+emilitogaza@users.noreply.github.com>
2026-09-07 14:11:27 +0200

    Add vintage champagne filter to product search

--- slices/Product/variations/product-search.tsx
+++ slices/Product/variations/product-search.tsx
@@ -46,6 +46,7 @@ export async function ProductSearch({ slice, context }: Props) {
         "product.product_style",
         "product.product_special_club",
         "product.product_ecologic",
+        "product.product_vintage",
         "product.product_volumes",
         "product.product_consumer_availability",
         "product.product_restaurant_availability",
